ec5711a992c3690a93cc855ed4a8b4d0fd6f1454
nater ahenjir <naternathan95@gmail.com>
2025-09-09 13:51:06 +0100

    solved all errors

--- app/Mail/OtpMail.php
+++ app/Mail/OtpMail.php
@@ -3,7 +3,6 @@
 namespace App\Mail;
 
 use Illuminate\Bus\Queueable;
-use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Mail\Mailable;
 use Illuminate\Mail\Mailables\Content;
 use Illuminate\Mail\Mailables\Envelope;
@@ -20,38 +19,23 @@ class OtpMail extends Mailable
         $this->otp = $otp;
     }
 
-    public function build()
-    {
-        return $this->subject('Your OTP Code')
-                    ->markdown('emails.otp')
-                    ->with(['otp'=>$this->otp]);
-    }
-
-    /**
-     * Get the message envelope.
-     */
     public function envelope(): Envelope
     {
         return new Envelope(
-            subject: 'Otp Mail',
+            subject: 'Your OTP Code',
         );
     }
 
-    /**
-     * Get the message content definition.
-     */
     public function content(): Content
     {
         return new Content(
             markdown: 'emails.otp',
+            with: [
+                'otp' => $this->otp,
+            ],
         );
     }
 
-    /**
-     * Get the attachments for the message.
-     *
-     * @return array<int, \Illuminate\Mail\Mailables\Attachment>
-     */
     public function attachments(): array
     {
         return [];
